3d2b1a4f2af20603c082c734db0eb6cfc702f648
Sam <sam.bantner@me.com>
2018-03-06 09:26:37 -0500

    Removed (and deleted) user secret

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 github_url="https://api.github.com/repos/BinaryBeard/xwingjson"
-user="BinaryBeard:ba02c518c427c3832a56ec58b2dc40e4949b44d7"
 
-download_urls=($(curl -u ${user} -s ${github_url}/releases/latest | jq --raw-output '.assets[] | .browser_download_url'))
+download_urls=($(curl -s ${github_url}/releases/latest | jq --raw-output '.assets[] | .browser_download_url'))
 version=$(echo ${download_urls} | grep -oE 'v\d\.\d\.\d')
 echo "Version: ${version}"
 
